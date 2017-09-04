2dc60b20e3419f516b50089c681e85b45e168200
Moses-Odessa <moses2804@gmail.com>
2017-09-04 17:00:28 +0300

    Рефакторинг получения даты загружаемого файла

---
+++ app/src/main/java/ua/moses/maximusprice/DataPriceEntry.java
@@ -0,0 +1,15 @@
+package ua.moses.maximusprice;
+
+import android.provider.BaseColumns;
+
+final class DataPriceEntry implements BaseColumns {
+    final static String TABLE_NAME = "pricelist";
+    final static String _ID = BaseColumns._ID;
+    final static String COLUMN_GOOD_ID = "good_id";
+    final static String COLUMN_NAME = "name";
+    final static String COLUMN_PRICE = "price";
+    final static String COLUMN_GROUP = "maingroup";
+    final static String COLUMN_SUBGROUP = "subgroup";
+    final static String COLUMN_DESCRIPTION = "description";
+    final static String COLUMN_AVALAIBILITY = "avalaibility";
+}
